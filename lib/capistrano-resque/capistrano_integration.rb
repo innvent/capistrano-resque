b95374a478175a89196cfe6aa295cec557d2d417
Luiz Costa <gutomcosta@gmail.com>
2016-06-28 15:06:38 -0300

    removes the configuration.instance checks

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -172,6 +172,6 @@ module CapistranoResque
   end
 end
 
-if Capistrano::Configuration.instance
-  CapistranoResque::CapistranoIntegration.load_into(Capistrano::Configuration.instance)
-end
+# if Capistrano::Configuration.instance
+#   CapistranoResque::CapistranoIntegration.load_into(Capistrano::Configuration.instance)
+# end
